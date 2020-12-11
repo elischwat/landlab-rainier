389e388490520cc112e6baf34f5efcf312ebbad1
elischwat <eli.l.schwat@gmail.com>
2020-12-11 12:30:07 -0800

    updates to historical hydrograph analysis notebook

--- Historical Carbon Watershed Hydrograph Analysis.py
+++ Historical Carbon Watershed Hydrograph Analysis.py
@@ -22,21 +22,18 @@ import os
 import matplotlib
 import numpy as np
 import matplotlib.pyplot as plt
-
-# import geopandas as gpd
 # from shapely.geometry import mapping
 # import matplotlib.pyplot as plt
 # import landlab
 # 
-from landlab.components import FlowAccumulator, KinwaveImplicitOverlandFlow, SinkFiller, SinkFillerBarnes
+from landlab.components import FlowAccumulator, OverlandFlow, \
+    KinwaveImplicitOverlandFlow, SinkFiller, SinkFillerBarnes
 # from landlab.components import KinwaveImplicitOverlandFlow, SinkFiller
 # from landlab.utils import get_watershed_masks_with_area_threshold
 # from landlab.utils import get_watershed_masks
 
 # import hsfm
 
-# import geopandas as gpd
-# from shapely.geometry import mapping
 # import seaborn as sns
 
 # import hvplot
@@ -44,7 +41,7 @@ from landlab.components import FlowAccumulator, KinwaveImplicitOverlandFlow, Sin
 # from pygeotools.lib import warplib
 # import gdal
 
-# import rasterio
+import rasterio
 
 
 # -
@@ -86,7 +83,54 @@ for file in dem_files:
         driver='AAIGrid',
     )
 
+# ## Get common-extent polygon
+
+from pygeotools.lib import geolib
+import gdal
+import shapely.wkt
+import geopandas as gpd
+
+dem_file_list = [
+    '73V3.asc',
+    '79V5.asc',
+    '90V3.asc',
+    '91V3.asc',
+    '92V3.asc',
+    '92V5.asc'
+]
+
+
+def get_polygon_from_raster_file(file_path, simplify_tolerance = 30):
+    ds = gdal.Open(file_path)
+    outline_ogr_polygon = geolib.get_outline(ds)
+    wkt = outline_ogr_polygon.ExportToWkt()
+    polygon = shapely.wkt.loads(wkt)
+    polygon = polygon.simplify(simplify_tolerance) # simplify to the same tolerance as the DEMs resolution
+    return polygon
+
+
+polygon_list = [
+    get_polygon_from_raster_file(
+        os.path.join(data_dir, dem_file)
+    ) 
+    for dem_file in dem_file_list
+]
+gdf = gpd.GeoDataFrame(
+    polygon_list, 
+    geometry=0, 
+    crs = gdal.Open(os.path.join(data_dir, dem_file_list[0])).GetProjection()
+)
+
+gdf.plot()
+
+(rmg, z) = read_esri_ascii(dem_path, name='topographic__elevation')
+imshow_grid(rmg, 'topographic__elevation')
+
+
+
 # ## Landlab It
+
+# 92V5 works!
 
 dem_path = os.path.join(data_dir, '73V3.asc')
 
@@ -97,7 +141,8 @@ rmg.set_nodata_nodes_to_closed(rmg.at_node['topographic__elevation'], -9999)
 imshow_grid(rmg, 'topographic__elevation')
 
 sfb = SinkFillerBarnes(
-    rmg
+    rmg,
+    fill_flat=False
 )
 
 sfb.run_one_step()
@@ -106,15 +151,11 @@ sfb.run_one_step()
 
 rmg.set_watershed_boundary_condition(z)
 
-outlet_node = rmg.set_watershed_boundary_condition(z, return_outlet_id=True, remove_disconnected=True)
-outlet_node
-
 fa = FlowAccumulator(rmg,
-                     surface='topographic__elevation',
+#                      surface='topographic__elevation',
                      flow_director='FlowDirectorD8',
 #                      runoff_rate=None,
-                     depression_finder='DepressionFinderAndRouter',
-#                      depression_finder=None
+#                      depression_finder='DepressionFinderAndRouter',
 #                      routing='D4'
                     )
 
@@ -125,13 +166,15 @@ outlet_node_to_sample = np.argmax(rmg.at_node['drainage_area'])
 print('Outlet Node = ' + str(outlet_node_to_sample) + '; Drainage Area= ' + 
   str(da[outlet_node_to_sample] / 1000000) + ' km^2; Elev = '+ str(round(z[outlet_node_to_sample], 1)) + ' m')
 
-# outlet-by-drainage-area, outlet-by-lowest-elevation
+# For each of the links, there is a tail and head and node.
+# Look at tail nodes of all links, and look at node area, find link that connects 
 
-outlet_node_to_sample, outlet_id
+ #the link number that carries the largest q
+outlet_link_to_sample = rmg.links_at_node[outlet_node_to_sample][3]
+
+rmg.links_at_node[outlet_node_to_sample]
 
 imshow_grid_at_node(rmg, 'drainage_area')
-
-imshow_grid_at_node(rmg, 'topographic__elevation')
 
 fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(6, 6))
 ax.xaxis.set_visible(False)
@@ -146,9 +189,9 @@ starting_precip_mmhr = 50    # [mm/hour] this is runoff rate uniformly distribut
 storm_duration = 1.0         # [hour]
 model_run_time = 4.0         # [hour]
 
-n = 0.1                     # Manning's roughness coefficient, (s/m^(1/3))
+n = 0.02                     # Manning's roughness coefficient, (s/m^(1/3))
 
-dt = 300                     # time step [sec] 
+dt = 1000                     # time step [sec] 
 
 #Converting units to SI [m] and [sec]
 storm_duration_sec=storm_duration*3600 # [sec]
@@ -159,9 +202,68 @@ discharge_at_outlet = []
 hydrograph_time = []
 rmg.add_zeros("surface_water__depth", at="node", clobber=True)
 
-kw = KinwaveImplicitOverlandFlow(rmg, runoff_rate=0.0, roughness=n, depth_exp=5/3)
+# ### Run/Loop Overland Flow Component
+
+alpha = 0.15           # time-step factor (nondimensional; from Bates et al., 2010) range 0.15-0.7
+starting_precip_ms = starting_precip_mmhr * (2.77778 * 10 ** -7)
+
+of = OverlandFlow(rmg, alpha=alpha, mannings_n=n, steep_slopes=True)
+
+# +
+elapsed_time = 1.0  # s
+
+while elapsed_time < model_run_time_sec:
+    # First, we calculate our time step.
+    of.dt = of.calc_time_step()
+    # Now, we can generate overland flow.
+     
+    ## The storm starts when the model starts. While the elapsed time is less
+    ## than the storm duration, we add water to the system as rainfall.
+    if elapsed_time < (storm_duration_sec):
+        of.rainfall_intensity = starting_precip_ms
+    else:  # elapsed time exceeds the storm duration, rainfall ceases.
+        of.rainfall_intensity = 0.0
+   
+            
+    of.run_one_step()
+    
+    # add elapsed time to the continuos time
+    hydrograph_time.append(elapsed_time/3600)
+    
+    # pass the surface water discharge to variable q. This includes unit q for all the links
+    q = rmg.at_link["surface_water__discharge"]
+    
+    #q is surface water discharge - divide by x-sectional area for fluid depth
+    # Search - does OverlandFlow provide depth to water
+    # can also calculate shear stress
+    
+    #append q for each location we would like to plot hydrograph and calculate Q=q*w.
+    discharge_at_outlet.append(np.abs(q[outlet_link_to_sample]) * rmg.dx)
+    
+
+     ## output time every now and then so that you know the code
+    ## is actually running
+    if (elapsed_time % 100) < 2:
+        print("elapsed time = ", elapsed_time)
+        
+     ## Updating elapsed_time  
+    elapsed_time += of.dt
+
+# +
+plt.figure(1)
+plt.plot(hydrograph_time, discharge_at_outlet, "r-", label="outlet")
+
+plt.ylabel("Discharge (cms)")
+plt.xlabel("Time (hour)")
+plt.legend(loc="upper right")
+title_text = f"Hydrograph ({dem_path.split('/')[-1].split('.')[0]})"
+plt.title(title_text)
+plt.show()
+# -
 
 # ### Run/Loop Kinematic Wave Overland Flow Component
+
+kw = KinwaveImplicitOverlandFlow(rmg, runoff_rate=0.0, roughness=n, depth_exp=5/3)
 
 # +
 elapsed_time=1;
